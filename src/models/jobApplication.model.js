f732f64dfb615803491c57c1fe7bd0e5ffa07770
Niharika <niharika384gupta@gmail.com>
2025-12-26 16:46:18 +0530

    fixed forwarded btn

--- src/models/jobApplication.model.js
+++ src/models/jobApplication.model.js
@@ -24,7 +24,7 @@ const jobApplicationSchema = new mongoose.Schema(
         "applied",
         "shortlisted",
         "rejected",
-        "forwareded",
+        "forwarded",
         "interview",
         "hired",
       ],
