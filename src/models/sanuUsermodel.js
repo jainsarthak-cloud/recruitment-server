3929922d48755b1fbb6dff213832dbc7867921da
knightwolf01 <sanukumarsingh387@gmail.com>
2026-01-29 17:19:30 +0530

    backend fix

--- src/models/sanuUsermodel.js
+++ src/models/sanuUsermodel.js
@@ -70,7 +70,7 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
-
+  
 userSchema.methods.comparePassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
