31f3b34793ccb20be0fa0bcdad98b0fa25b39c0e
raj raghuwanshi <rajraghuawnshi9479@gmail.com>
2026-02-12 13:41:23 +0530

    CURD operations with Client

--- src/app.js
+++ src/app.js
@@ -30,6 +30,7 @@ import passwordRoutes from "./routes/password.routes.js";
 import ViolationRoutes  from "./routes/testViolation.routes.js";
 import shareCandidate from "./routes/shareCandidate.routes.js";
 import tokenRoutes from "./routes/token.route.js"
+import clientRoutes from "./routes/client.routes.js"
 const app = express();
 app.set("trust proxy", 1);
 app.use(express.json());
@@ -65,6 +66,7 @@ app.use("/api/password", passwordRoutes);
 app.use('/api/ai/', ViolationRoutes)
 app.use('/api/share', shareCandidate);
 app.use("/api/token",tokenRoutes)
+app.use("/api/client",clientRoutes)
 
 app.use(errorHandler);
 export default app;
