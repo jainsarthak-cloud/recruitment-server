42c684332f8330f77666cc756cc99f99c9d96595
Your Name <prashantsinghberath@gmail.com>
2026-02-14 17:21:31 +0530

    fix: certificate CRUD

--- src/routes/certificate.routes.js
+++ src/routes/certificate.routes.js
@@ -22,7 +22,11 @@ router.get("/", certificateController.listAll);
 router.get("/:id", certificateController.get);
 
 // Update Cretificate
-router.put("/update/:id", updateCertificateValidator, certificateController.update);
+router.put(
+  "/update/:id",
+  updateCertificateValidator,
+  certificateController.update,
+);
 
 // Delete Cretificate
 router.delete("/:id", certificateController.delete);
