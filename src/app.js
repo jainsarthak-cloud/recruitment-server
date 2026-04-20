77078460a33874bbb6414fa5ce1539bf1815c0af
mayank kumar <mayankkumar950844@gmail.com>
2026-04-20 15:51:34 +0530

    feat(job-search): add searchJobRole and searchLocation API

--- src/app.js
+++ src/app.js
@@ -27,7 +27,6 @@ import savedJobRoutes from "./routes/savedJob.routes.js";
 import passwordRoutes from "./routes/password.routes.js";
 import jobApplicationQuesition from "./routes/jobApplicationQuesition.route.js";
 // import deleteTestsRoutes from "./routes/delete-tests.routes.js";
-import productRoutes from "./routes/product.routes.js";   // new route for products
 
 import adminProfileRoutes from "./routes/adminProfile.routes.js";
 
@@ -79,7 +78,6 @@ app.use("/api/blogs", blogPostRoutes);
 
 
 app.use("/api/categories", categoryRoutes);
-app.use("/api/products", productRoutes);   // new route for products
 
 
 app.use(errorHandler);
